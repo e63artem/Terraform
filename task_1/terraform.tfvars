547cca9fa1157234c1a852136245865260d9e254
Evseev Artem <evseev4artem@gmail.com>
2024-03-16 04:27:50 +0400

    .

---
+++ task_1/terraform.tfvars
@@ -0,0 +1,3 @@
+user_name  = "<my_user>"
+user_pass  = "<my_password>"
+admin_pass = "<my_password>"
---
+++ task_1/variables.tf
@@ -0,0 +1,77 @@
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "network" {
+  type    = string
+  default = "ya-network"
+}
+
+variable "subnet" {
+  type    = string
+  default = "ya-network"
+}
+
+variable "subnet_v4_cidr_blocks" {
+  type    = list(string)
+  default = ["192.168.0.0/16"]
+}
+
+variable "nat" {
+  type    = bool
+  default = true
+}
+
+variable "image_family" {
+  type    = string
+  default = "ubuntu-2204-lts"
+}
+
+variable "name" {
+  type = string
+  default = "test"
+}
+
+variable "cores" {
+  type    = number
+  default = 2
+}
+
+variable "memory" {
+  type    = number
+  default = 4
+}
+
+variable "disk_size" {
+  type    = number
+  default = 50
+}
+
+variable "disk_type" {
+  type    = string
+  default = "network-nvme"
+}
+
+variable "user_name" {
+  default = ""
+  type    = string
+}
+
+variable "user_pass" {
+  default = ""
+  type    = string
+}
+
+variable "admin_pass" {
+  default = ""
+  type    = string
+}
+
+variable "timeout_create" {
+  default = "10m"
+}
+
+variable "timeout_delete" {
+  default = "10m"
+}
